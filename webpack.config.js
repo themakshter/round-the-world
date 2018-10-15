90cd7d23e01d8217cfa3e6fdd1b9fbcd52988f58
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-10-16 00:58:33 +0200

    Load image by folder not one by one (#26)
    This PR addresses the issue #25 
    
    The core of it is:
    
    - use file loader to tell webpack how to process file
    - configure fileloader not to move the file / hash them
    - use require context to scan flags and audio folder to extract all the files
    - manipulate the filename to match the keys and use `this.game.load` to load the files
    
    from now on all the files dropped in that folder will be loaded in the game with a key equal to their name minus the file extension.
    
    I know that I left out few files but I felt like that since they are diverse their handling is better in the hands of the authors, but I believe is easy now to proceed on that pat.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|ogg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: false
+            }
+          }
+        ]
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'round-the-world-game.min.js',
